5601854069cf4bfdd54f27b4c0a84d9ecfa39ed2
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-11-11 18:10:41 -0600

    began styling

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,14 +1,15 @@
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/other">Other</RouterLink>
+    <RouterLink to="/">Services</RouterLink>
+    <RouterLink to="/">Contact Us</RouterLink>
+    <RouterLink to="/">Login</RouterLink>
   </nav>
 </template>
 
 <style lang="postcss" scoped>
 nav {
-  @apply flex justify-center space-x-4;
-
+  @apply flex items-end justify-end space-x-4 px-5 py-9 text-slate-300;
   & .router-link-active {
     @apply underline underline-offset-4;
   }
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Hello There ✋')
+const title = ref('Hernandez Car Care 🚗')
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
+  <main class="flex items-start justify-start">
+    <h1 class="text-6xl font-bold text-white">{{ title }}</h1>
   </main>
 </template>
