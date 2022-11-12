b0a839f9c4f2d8c25a70d98d0c58dc41fe625d99
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-11-11 18:18:55 -0600

    more styling

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Hernandez Car Care 🚗')
+const title = ref('Hernandez Car Care')
 </script>
 
 <template>
   <main class="flex items-start justify-start">
-    <h1 class="text-6xl font-bold text-white">{{ title }}</h1>
+    <h1 class="px-5 font-mono text-6xl font-bold italic text-white">{{ title }}</h1>
   </main>
 </template>
