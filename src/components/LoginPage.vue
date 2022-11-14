a21080ec50fc55a71cd86ffd6272519fe52762cd
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-11-13 18:06:25 -0600

    added styling

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,37 +1,30 @@
 <script setup>
 import { ref } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
 
-const router = useRouter()
-const route = useRoute()
 const username = ref('')
 const password = ref('')
-const logUserIn = async () => {
-  if (await login(username.value, password.value)) {
-    if (route.query.redirect) {
-      router.push(route.query.redirect)
-    } else {
-      router.push({ name: 'Home' })
-    }
-  } else {
-    logout()
-  }
-}
 </script>
 
 <template>
   <form class="login-form" @submit.prevent="logUserIn">
-    <input v-model="username" type="text" placeholder="Username" />
+    <input v-model="username" type="text" placeholder="Email Address" />
     <input v-model="password" type="password" placeholder="Password" />
-    <button type="submit" class="bg-white px-4 py-2">Log In</button>
+    <button type="submit" class="submit">Log In</button>
+    <button type="sign-up" class="register">New user? Sign up</button>
   </form>
 </template>
 
 <style scoped lang="postcss">
 .login-form {
-  @apply mx-auto mt-56 flex max-w-lg flex-col gap-4 rounded-md bg-white p-3 shadow-lg;
+  @apply mx-auto mt-56 flex max-w-lg flex-col gap-4 rounded-md p-3 shadow-lg;
   & input {
-    @apply rounded-md py-2 px-4 text-xl ring-1 ring-slate-600;
+    @apply rounded-md border-b-4 border-b-black py-2 px-4 text-center text-xl ring-1 ring-black;
   }
+}
+.submit {
+  @apply mx-44 mt-5 rounded-lg bg-yellow-500 py-2 px-4 text-xl font-semibold text-slate-300 shadow-lg hover:bg-white hover:text-yellow-500;
+}
+.register {
+  @apply mt-3 text-white;
 }
 </style>
