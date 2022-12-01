061135207ce80f4c85721fbca666f8618b2a8d46
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-11-30 22:55:12 -0600

    styled book now page

--- src/components/BookNow.vue
+++ src/components/BookNow.vue
@@ -1,7 +1,19 @@
 <script setup></script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">Book Now</h1>
-  </main>
+  <div class="wrapper">
+    <h1>GET THAT NEW CAR SHINE</h1>
+  </div>
+  <div class="message">
+    <p>If you would like to book an appointment with us, fill out the form below.</p>
+  </div>
 </template>
+
+<style lang="postcss" scoped>
+.wrapper {
+  @apply text-center text-3xl font-bold text-white;
+}
+.message {
+  @apply py-4 text-center text-base font-medium italic text-white;
+}
+</style>
